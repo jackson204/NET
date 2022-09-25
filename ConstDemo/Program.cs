749c11030fbfb1cfaadf81ca0a44c901a6ed15ca
jackson <s97403002a@gmail.com>
2022-09-25 10:39:53 +0800

    add const

---
+++ ConstDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConstDemo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int number = 10;
+            number = 20; // 變量 可重新賦值
+
+            const int number2 = 20; //常量 不能被重新賦值
+            //number2 = 30;
+            
+            
+        }
+    }
+}
