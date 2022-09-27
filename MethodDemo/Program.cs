9966a8c44d2a00b5797f0d9e76c579d97bdf36e3
jackson <s97403002a@gmail.com>
2022-09-27 22:05:12 +0800

    add Method

---
+++ MethodDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MethodDemo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var sum = Program.Sum(1,2);
+            Console.WriteLine(sum);
+            
+        }
+
+        private static int Sum(int i, int j)
+        {
+            return i + j;
+        }
+    }
+}
