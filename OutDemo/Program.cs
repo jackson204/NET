6b88e8d64caac57e5ed37db0d6547925aae50b3a
jackson <s97403002a@gmail.com>
2022-09-29 21:15:39 +0800

    add out

---
+++ OutDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.CodeDom;
+
+namespace OutDemo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Test(out var max, out var min);
+            Console.WriteLine($"max {max}  min {min}");
+            
+            var tryParse = int.TryParse("123a" , out var s);
+
+            Console.WriteLine($"tryParse {tryParse} , result {s}");
+            
+            
+            var myTryParse = MyTryParse("123a", out var result);
+            Console.WriteLine($"myTryParse {myTryParse} , result {result}");
+
+           
+        }
+
+        private static bool MyTryParse(string input, out int result)
+        {
+            result = 0;
+            try
+            {
+                result = int.Parse(input);
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        private static void Test(out int max, out int min)
+        {
+            max = 2;
+            min = 1;
+        }
+    }
+}
