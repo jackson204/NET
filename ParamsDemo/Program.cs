6b83cb9d2f23b4f2dd91e900435261928d783421
jackson <s97403002a@gmail.com>
2022-10-02 16:28:37 +0800

    add Params

---
+++ ParamsDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.CodeDom;
+
+namespace ParamsDemo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var result = Sum(1,2);
+            Console.WriteLine(result);
+            var result2 = Sum(1,2,3,4);
+            Console.WriteLine(result2);
+        }
+
+        private static int Sum(params int[] lists)
+        {
+            int result = 0;
+            for(int i = 0; i < lists.Length; i++)
+            {
+                 result += lists[i];
+            }
+            return result;
+        }
+    }
+}
