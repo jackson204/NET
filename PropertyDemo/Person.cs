0c63cf195064124b0d94f20ecdb5d2596369ebf4
jackson <s97403002a@gmail.com>
2022-10-11 21:25:07 +0800

    add 屬性

---
+++ PropertyDemo/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace PropertyDemo
+{
+    internal class Person
+    {
+        //存取數據
+        private int _age;
+
+        public int Age
+        {
+            // 相當於
+            //     public int Get_age()
+            // {
+            //     return this._age;
+            // }
+            get
+            {
+                return _age;
+            } 
+            //     相當於
+            //     public void Set_age(int value)
+            // {
+            //     _age = value;
+            // }
+            //當給屬性賦值的時候
+            set
+            {
+                if (value < 0 || value > 100)
+                {
+                    value = 0;
+                }
+                _age = value;
+            }
+        }
+
+        public void CHCSS()
+        {
+            Console.WriteLine(Age);
+        }
+    }
+}
---
+++ PropertyDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+
+namespace PropertyDemo
+{
+    internal class Program
+    {
+        //屬性的作用就是保護字段，對字段的賦值與取值進行限定
+        //屬性的本質就是兩個方法，get(),set()
+        public static void Main(string[] args)
+        {
+            //需用debug模式觀看，程式碼如何運行
+            var person = new Person();
+            person.Age = -12;
+            person.CHCSS();
+          
+        }
+    }
+}
