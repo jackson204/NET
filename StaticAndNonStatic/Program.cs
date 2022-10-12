d598f039aae8b442a0b6a5fca6f3b635d63698b3
jackson <s97403002a@gmail.com>
2022-10-13 00:28:34 +0800

    add static and nonStatic

---
+++ StaticAndNonStatic/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Security.Principal;
+
+namespace StaticAndNonStatic
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var person = new Person();
+            person.M1();
+            Person.M2();
+            Student.M3();
+        }
+    }
+
+    internal class Person
+    {
+        public void M1()
+        {
+            Console.WriteLine("non static");
+        }
+
+        public static void M2()
+        {
+            Console.WriteLine("Static");
+        }
+    }
+    public static class Student{
+        
+        private static string _name;
+
+        public static string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        public static void M3()
+        {
+            Console.WriteLine("Static Class");
+        }
+    }
+}
