0ecb215a297eb9de6a45399da91b9d7e95d728b9
jackson <s97403002a@gmail.com>
2022-10-13 22:59:58 +0800

    add 建構式 this

---
+++ ConstrorDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Security.Permissions;
+
+namespace ConstrorDemo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var student = new Student("BBB" );
+            Console.WriteLine($"{student.Name} , {student.Age}");
+        }
+    }
+}
---
+++ ConstrorDemo/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConstrorDemo
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public Student(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+            Console.WriteLine("(name , age )");
+        }
+        public Student(string name):this(name,10)
+        {
+            Console.WriteLine("name");
+        }
+    }
+}
