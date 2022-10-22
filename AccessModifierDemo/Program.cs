234cd46dcc9359c04ecd218e4eb995de0a0179a9
jackson <s97403002a@gmail.com>
2022-10-22 16:24:49 +0800

    Add protected

---
+++ AccessModifierDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Security.AccessControl;
+
+namespace AccessModifierDemo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var person = new Person();
+        }
+    }
+
+    public class Person
+    {
+        // 子類可以訪問的到
+        protected string _name;
+
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+    }
+
+    public class Student : Person
+    {
+        public void Test()
+        {
+            Console.WriteLine(this._name);
+        }
+    }
+}
