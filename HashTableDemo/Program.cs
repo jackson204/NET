1f7de00bf75afeb846428b42f5ccc524467d1551
jackson <s97403002a@gmail.com>
2022-10-26 21:50:34 +0800

    add Hashtable

---
+++ HashTableDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+
+namespace HashTableDemo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var hashtable = new Hashtable();
+            hashtable.Add("A", "ZZZ");
+            hashtable.Add(1, true);
+            hashtable.Add(false, 222);
+
+            foreach (DictionaryEntry item in hashtable)
+            {
+                Console.WriteLine($"Key : {item.Key} Value {item.Value} ");
+            }
+        }
+    }
+}
