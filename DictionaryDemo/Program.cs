e5f2a321e26277a465ad4bf993f16e752de9ec83
jackson <s97403002a@gmail.com>
2022-10-29 14:30:58 +0800

    add Dictionary

---
+++ DictionaryDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryDemo
+{
+  internal class Program
+  {
+    public static void Main(string[] args)
+    {
+      var dictionary = new Dictionary<int, string>()
+      {
+        {1,"AA"} ,
+        {2,"B"},
+      };
+      foreach (KeyValuePair<int,string> pair in dictionary)
+      {
+        Console.WriteLine($"{pair.Key} , {pair.Value}");
+      }
+    }
+  }
+}
