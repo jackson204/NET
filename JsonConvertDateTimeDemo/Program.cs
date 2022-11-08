fee596e3b18c7d44655e3e62c32069d84ff28901
jackson <s97403002a@gmail.com>
2022-11-08 22:59:01 +0800

    JsonConvert 時區轉換

---
+++ JsonConvertDateTimeDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace JsonConvertDateTimeDemo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var input = "{\"Time\":\"2022/07/07 07:39:17.19-04:00\",\"DateTimeOffset\":\"2022/07/07 07:39:17.19-04:00\"}";
+            var value = JsonConvert.DeserializeObject<MyClass>(input);
+            Console.WriteLine($"time            {value.Time}");
+            Console.WriteLine($"DateTimeOffset  {value.DateTimeOffset}");
+            Console.WriteLine($"UtcTime         {value.UtcTime}");
+        }
+    }
+
+    internal class MyClass
+    {
+        public DateTime Time { get; set; }
+
+        public DateTimeOffset DateTimeOffset { get; set; }
+
+        public DateTime UtcTime => DateTimeOffset.UtcDateTime;
+    }
+}
---
+++ JsonConvertDateTimeDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("JsonConvertDateTimeDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JsonConvertDateTimeDemo")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0F769565-518D-41CB-BA51-B8689A8E0CEE")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
