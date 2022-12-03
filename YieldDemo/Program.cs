4757787564a0ea6ce03859ab9d324e4c2a7fbdee
jackson <s97403002a@gmail.com>
2022-12-03 23:09:18 +0800

    add yield 搭配 IEnumerable

---
+++ YieldDemo/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace YieldDemo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Good> list = new List<Good>()
+            {
+                new Good()
+                {
+                    Name = "Test1", Age = 10
+                },
+                new Good()
+                {
+                    Name = "Test2", Age = 20
+                },
+                new Good()
+                {
+                    Name = "Test3", Age = 30
+                },
+                new Good()
+                {
+                    Name = "Test4", Age = 40
+                },
+            };
+
+           var item =  GetGood(list, 25);
+
+           foreach (var good in item)
+           {
+               Console.WriteLine(good.Age);
+           }
+        }
+
+        private static IEnumerable<Good> GetGood(List<Good> list, int age)
+        {
+            foreach (var item in list)
+            {
+                if (item.Age > age)
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+
+    internal class Good
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
