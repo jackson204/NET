42f8712f7f43eed9a093f96ae930993190aea7fd
jackson <s97403002a@gmail.com>
2022-12-13 21:15:43 +0800

    Select

--- ExecuteSqlCommandDemo/Program.cs
+++ ExecuteSqlCommandDemo/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data.Entity;
+using System.Linq;
 
 namespace ExecuteSqlCommandDemo
 {
@@ -9,18 +11,38 @@ namespace ExecuteSqlCommandDemo
             using (var db = new MyDbContext())
             {
                 db.Database.Log = s => Console.WriteLine(s);
-                var sql = @"create table dbo.ProductUser
-                (
-                    id       int not null
-                constraint id
-                primary key,
-                    account  varchar(10),
-                password varchar(10)
-                    )
-                ";
-                var result = db.Database.ExecuteSqlCommand(sql);
-                Console.WriteLine(result);
+                //Ceate Table
+                // var sql = @"create table dbo.ProductUser
+                // (
+                //     id       int not null
+                // constraint id
+                // primary key,
+                //     account  varchar(10),
+                // password varchar(10)
+                //     )
+                // ";
+                
+                //查詢
+                var sql =$@"SELECT TOP (1000) [id]
+                    ,[account]
+                ,[password]
+                FROM [MyDB2].[dbo].[ProductUser]";
+                
+                
+                var result = db.Database.SqlQuery<ProductUser>(sql);
+                var productUser = result.FirstOrDefault(r => r.id ==1);
+            
+                Console.WriteLine(productUser.account);
             }
         }
+    }
+
+    internal class ProductUser
+    {
+        public int id { get; set; }
+
+        public string account { get; set; }
+
+        public string password { get; set; }
     }
 }
