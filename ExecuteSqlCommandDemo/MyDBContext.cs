4f20679f40314cc7adb6b9f6bb4299e89ee97653
jackson <s97403002a@gmail.com>
2022-12-13 10:08:49 +0800

    下 sql 建立表

---
+++ ExecuteSqlCommandDemo/MyDBContext.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+namespace ExecuteSqlCommandDemo
+{
+    public partial class MyDbContext : DbContext
+    {
+        public MyDbContext()
+            : base("name=MyDBEntities2")
+        {
+        }
+        
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
---
+++ ExecuteSqlCommandDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ExecuteSqlCommandDemo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var db = new MyDbContext())
+            {
+                var sql = @"create table dbo.ProductUser
+                (
+                    id       int not null
+                constraint id
+                primary key,
+                    account  varchar(10),
+                password varchar(10)
+                    )
+                ";
+                var result = db.Database.ExecuteSqlCommand(sql);
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
