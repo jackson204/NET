81a39f14507567b0d2293c26165b6a9dfeba6f72
jackson <s97403002a@gmail.com>
2022-12-14 21:39:35 +0800

    virtual including

--- CodeFirstDemo/Program.cs
+++ CodeFirstDemo/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using CodeFirstDemo.Model;
@@ -11,36 +10,61 @@ namespace CodeFirstDemo
         //1 user 對應 多個 order
         public static void Main(string[] args)
         {
-            using (var _db = new MyDbContext())
+            using (var db = new MyDbContext())
             {
-                var user = new User()
-                {
-                    Email = "AA",
-                    Pwd = "123456",
-                    LoginTime = DateTime.Now,
-                    Orders = new List<Order>()
-                    {
-                        new Order()
-                        {
-                            Address = "Address",
-                            Message = "Message",
-                            Phone = "89841310",
-                            Status = "Status",
-                        },
-                        new Order()
-                        {
-                            Address = "2Address2",
-                            Message = "2Message2",
-                            Phone = "298413102",
-                            Status = "2Status2",
-                        },
-                    }
-                
-                    // user.Orders
-                };
-                 _db.Users.Add(user);
-                _db.SaveChanges();
+                db.Database.Log = Console.WriteLine;
 
+                #region 新增
+
+                // var user = new User()
+                // {
+                //     Email = "AA",
+                //     Pwd = "123456",
+                //     LoginTime = DateTime.Now,
+                //     Orders = new List<Order>()
+                //     {
+                //         new Order()
+                //         {
+                //             Address = "Address",
+                //             Message = "Message",
+                //             Phone = "89841310",
+                //             Status = "Status",
+                //         },
+                //         new Order()
+                //         {
+                //             Address = "2Address2",
+                //             Message = "2Message2",
+                //             Phone = "298413102",
+                //             Status = "2Status2",
+                //         },
+                //     }
+                //
+                //     // user.Orders
+                // };
+                // _db.Users.Add(user);
+                // _db.SaveChanges();
+
+                #endregion
+
+                #region virtual 延遲加載
+
+                // var user = db.Users.FirstOrDefault(r=>r.Id==1);
+                // Console.WriteLine(user.Email);
+
+                // var userOrders = user.Orders;
+                // foreach (Order order in userOrders)
+                // {
+                //     Console.WriteLine($"order Message {order.Message}");
+                // } 
+
+                #endregion
+
+                #region including
+
+                var reuslt = db.Users.Include(r => r.Orders).FirstOrDefault(r=>r.Id==1);
+                Console.WriteLine(reuslt);
+
+                #endregion
             }
         }
     }
