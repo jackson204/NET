2d3cdc6822f8c982cb3a2e13af59833623c190c9
jackson <s97403002a@gmail.com>
2022-12-16 23:14:42 +0800

    adjust

--- CodeFirstDemo/Program.cs
+++ CodeFirstDemo/Program.cs
@@ -20,25 +20,25 @@ namespace CodeFirstDemo
                 var user = new User()
                 {
                     Email = "AA",
-                    // Pwd = "123456",
-                    // LoginTime = DateTime.Now,
-                    // Orders = new List<Order>()
-                    // {
-                    //     new Order()
-                    //     {
-                    //         Address = "Address",
-                    //         Message = "Message",
-                    //         Phone = "89841310",
-                    //         Status = "Status",
-                    //     },
-                    //     new Order()
-                    //     {
-                    //         Address = "2Address2",
-                    //         Message = "2Message2",
-                    //         Phone = "298413102",
-                    //         Status = "2Status2",
-                    //     },
-                    // }
+                    Pwd = "123456",
+                    LoginTime = DateTime.Now,
+                    Orders = new List<Order>()
+                    {
+                        new Order()
+                        {
+                            Address = "Address",
+                            Message = "Message",
+                            Phone = "89841310",
+                            Status = "Status",
+                        },
+                        new Order()
+                        {
+                            Address = "2Address2",
+                            Message = "2Message2",
+                            Phone = "298413102",
+                            Status = "2Status2",
+                        },
+                    }
                 
                     // user.Orders
                 };
