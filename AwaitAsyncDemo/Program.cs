fe63b9782c08e29ad81afc2ea6711b2340b5c8ba
jackson <s97403002a@gmail.com>
2022-12-23 23:06:33 +0800

    Await Async

---
+++ AwaitAsyncDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AwaitAsyncDemo
+{
+    class Program
+    {
+
+    // 1.異步方法的返回值一般是Task<T>, T是真正的返回值類型
+    // 2.有 await 方法必須修視為 async
+    static async Task Main(string[] args)
+    {
+        var fileName = @"C:\Users\s9740\Desktop\test.txt";
+
+        var sb = new StringBuilder();
+        for(var i = 0; i < 10000; i++)
+        {
+            sb.AppendLine(i + "Hello");
+        }
+
+        //File.WriteAllTextAsync(fileName, sb.ToString());
+        await File.WriteAllTextAsync(fileName, sb.ToString());
+        var readAllTextAsync = await File.ReadAllTextAsync(fileName);
+        Console.WriteLine(readAllTextAsync);
+    }
+}
+
+}
