a15c33e0b2b981d74376feca39e47964e4049842
jackson <s97403002a@gmail.com>
2022-12-24 16:12:07 +0800

    使用Async 但是外層方法不使用Async

---
+++ AwaitAsyncDemo1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace AwaitAsyncDemo1
+{
+    class Program
+    {
+        //Main() 不支持異部方法 使用Result
+        static void Main(string[] args)
+        {
+            var fileName = @"C:\Users\s9740\Desktop\test.txt";
+            var readAllTextAsync = File.ReadAllTextAsync(fileName).Result;
+            Console.WriteLine(readAllTextAsync);
+        }
+    }
+}
