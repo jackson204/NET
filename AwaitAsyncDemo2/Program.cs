a1a8401aba17e21e90e44cf53c58515fa8e2801e
jackson <s97403002a@gmail.com>
2022-12-24 21:03:34 +0800

    異部方法不等於多線誠

---
+++ AwaitAsyncDemo2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace AwaitAsyncDemo2
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("start  = " + Thread.CurrentThread.ManagedThreadId);
+            // double r = await CalcAsync(500);
+            double r = await CalcAsync2(500);
+            Console.WriteLine($"r = {r}");
+            Console.WriteLine("end = " + Thread.CurrentThread.ManagedThreadId);
+        }
+
+        private static async Task<double> CalcAsync(int n)
+        {
+            Console.WriteLine("CalcAsync = "+Thread.CurrentThread.ManagedThreadId);
+            double result = 0;
+            for(int i = 0; i < 10000000; i++)
+            {
+                result += i;
+            }
+            return result;
+        }
+        private static async Task<double> CalcAsync2(int n)
+        {
+            return await Task.Run(() =>
+            {
+                Console.WriteLine("CalcAsync = " + Thread.CurrentThread.ManagedThreadId);
+                double result = 0;
+                for(int i = 0; i < 10000000; i++)
+                {
+                    result += i;
+                }
+                return result;
+            });
+
+        }
+    }
+}
