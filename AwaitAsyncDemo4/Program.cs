eaaf0258e8513053d93b6286b5184f1386cf70dd
jackson <s97403002a@gmail.com>
2022-12-25 14:39:56 +0800

    WhenAll

---
+++ AwaitAsyncDemo4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AwaitAsyncDemo4
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var t1 = File.ReadAllTextAsync(@"C:\Users\s9740\Desktop\test.txt");
+            var t2 = File.ReadAllTextAsync(@"C:\Users\s9740\Desktop\test.txt");
+            var t3 = File.ReadAllTextAsync(@"C:\Users\s9740\Desktop\test.txt");
+            var task = await Task.WhenAll(t1,t2,t3);
+            Console.WriteLine(task[0]);
+        }
+    }
+}
