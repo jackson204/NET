0bf5f8efb01ecfedae102c637a49d7ed4e4f3a4b
jackson <s97403002a@gmail.com>
2022-12-25 14:03:30 +0800

    沒有async的異步方法

---
+++ AwaitAsyncDemo3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AwaitAsyncDemo3
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var result = await ReadFileAsync(1);
+            Console.WriteLine(result);
+        }
+
+        //沒有async的異步方法
+        private static Task<string> ReadFileAsync(int i)
+        {
+            if (i ==1)
+            {
+                return File.ReadAllTextAsync(@"C:\Users\s9740\Desktop\test.txt");
+            }
+            throw new Exception();
+        }
+    }
+}
