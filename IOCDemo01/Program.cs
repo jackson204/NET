3a8fd1f260f9e3c5f2563fc1242768ef6759fc4b
jackson <s97403002a@gmail.com>
2023-01-05 23:55:43 +0800

    first DI

---
+++ IOCDemo01/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IOCDemo01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var testServiceImpl = new TestServiceImpl();
+            // testServiceImpl.Name = "Tom";
+            // testServiceImpl.SayHi();
+
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddTransient<TestServiceImpl>();
+            using (var buildServiceProvider = serviceCollection.BuildServiceProvider())
+            {
+                var testServiceImpl = buildServiceProvider.GetService<TestServiceImpl>();
+                testServiceImpl.Name = "HH";
+                testServiceImpl.SayHi();
+            }
+           
+        }
+    }
+
+    public interface ITestService
+    {
+        public string Name {
+            get;
+            set;
+        }
+
+        public void SayHi();
+    }
+
+    public class TestServiceImpl : ITestService
+    {
+        public string Name { get; set; }
+
+        public void SayHi()
+        {
+            Console.WriteLine($"Hi i {Name}");
+        }
+        
+    }
+    public class TestServiceImpl2 : ITestService
+    {
+        public string Name { get; set; }
+
+        public void SayHi()
+        {
+            Console.WriteLine($"Hi TestServiceImpl2i {Name}");
+        }
+        
+    }
+    
+}
