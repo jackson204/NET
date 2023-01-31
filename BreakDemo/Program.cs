be51c41e51b9c787daf250c330d642c0ac3d62aa
jackson <s97403002a@gmail.com>
2023-01-31 21:59:04 +0800

    add break

---
+++ BreakDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BreakDemo
+{
+    internal class Program
+    {
+        /// <summary>
+        /// while 跳出當前循環 
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            int i = 1;
+            int j = 1;
+            while(i < 10)
+            {
+                while(j < 10)
+                {
+                    Console.WriteLine("in");
+                    j++;
+                    break;
+                }
+                Console.WriteLine("out");
+                i++;
+            }
+        }
+    }
+}
