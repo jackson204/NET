4819c304520a886759c52b48f123e518905192e5
jackson <s97403002a@gmail.com>
2023-02-11 16:58:57 +0800

    add enum

--- EnumDemo/Program.cs
+++ EnumDemo/Program.cs
@@ -10,7 +10,8 @@ namespace EnumDemo
             女
         }
 
-        public enum QQState
+        // 默認情況下，每個枚舉值的基數類型是 int
+        private enum QqState
         {
             OnLine,
             OffLine = 3,
@@ -21,29 +22,32 @@ namespace EnumDemo
         public static void Main(string[] args)
         {
             #region enum 強轉成 int
+
             // 變量類型  變量名 = 值
-            QQState qqState = QQState.Busy;
+            QqState qqState = QqState.Busy;
+
+            //enum 默認是可以跟int 互相轉換 
             int n = (int) qqState;
-            Console.WriteLine((int) QQState.OnLine); // 從0開始
-            Console.WriteLine((int) QQState.OffLine);
-            Console.WriteLine((int) QQState.Leave);
-            Console.WriteLine((int) QQState.Busy);
+            Console.WriteLine((int) QqState.OnLine); // 從0開始
+            Console.WriteLine((int) QqState.OffLine);
+            Console.WriteLine((int) QqState.Leave);
+            Console.WriteLine((int) QqState.Busy);
 
             #endregion
 
             #region int 轉成 enum
 
             int a = 3;
-            Console.WriteLine((QQState) a);
+            Console.WriteLine((QqState) a);
 
             a = 8; //不再enum 的 則  吐原值
-            Console.WriteLine((QQState) a);
+            Console.WriteLine((QqState) a);
 
             #endregion
 
             #region enum 轉 string
 
-            QQState state = QQState.Busy;
+            QqState state = QqState.Busy;
             var format = state.ToString();
             Console.WriteLine(format);
 
@@ -52,16 +56,17 @@ namespace EnumDemo
             #region string 轉 enum
 
             string name = "Leave";
-            QQState o = (QQState) Enum.Parse(typeof(QQState), name);
-            Console.WriteLine(o);
-
-            name = "9"; //不再enum 的 則  吐原值
-            o = (QQState) Enum.Parse(typeof(QQState), name);
+            Type enumType = typeof(QqState);
+            QqState o = (QqState) Enum.Parse(enumType, name);
             Console.WriteLine(o);
             
+            name = "9"; //不再enum 的 則  吐原值
+            o = (QqState) Enum.Parse(typeof(QqState), name);
+            Console.WriteLine(o);
+
             // 如果無法轉換則抱錯
             name = "ASDS";
-            o = (QQState) Enum.Parse(typeof(QQState), name);
+            o = (QqState) Enum.Parse(typeof(QqState), name);
             Console.WriteLine(o);
 
             #endregion
