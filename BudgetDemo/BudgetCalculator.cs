c3b67895e1a02d7d4447f792930f78c2277387f2
jackson <s97403002a@gmail.com>
2023-02-27 15:54:59 +0800

    TDD:Step9 Refactor

--- BudgetDemo/BudgetCalculator.cs
+++ BudgetDemo/BudgetCalculator.cs
@@ -14,59 +14,77 @@ namespace BudgetDemo
 
         public decimal CalculateBudget(DateTime start, DateTime end)
         {
-            decimal budgets;
-
-            if (SameMonth(start, end))
-            {
-                //同月 
-                //1.找出預算
-                var budget = QueryMonthlyBudget(start);
-
-                //2.找出當月天數
-                var daysInMonth = FindDaysInMonth(start);
-
-                //3.分配給每天 與 計算天數
-                budgets = budget.Amount / daysInMonth * ((end - start).Days + 1);
-
-                return budgets;
-            }
-            else
-            {
-                // 計算開始月份的預算
-                var startBudget = QueryMonthlyBudget(start);
-
-                //2.找出當月天數
-                var daysInMonth = FindDaysInMonth(start);
-
-                //3.分配給每天 與 計算天數
-                budgets = (startBudget.Amount / daysInMonth) * ((daysInMonth - start.Day) + 1);
-
-                // 計算中間月份的預算
-                var endDate = new DateTime(end.Year, end.Month, 1);
-                for(var month = start.AddMonths(1); month < endDate; month = month.AddMonths(1))
-                {
-                    var budget = QueryMonthlyBudget(month);
-                    if (budget != null)
-                    {
-                        daysInMonth = DateTime.DaysInMonth(month.Year, month.Month);
-                        budgets += budget.Amount / daysInMonth * daysInMonth;
-                    }
-                }
-
-                // 計算結束月份的預算
-                var endBudget = QueryMonthlyBudget(end);
-
-                //2.找出當月天數
-                var endDaysInMonth = FindDaysInMonth(end);
-
-                //3.分配給每天 與 計算天數
-                var endBudgetAmount = endBudget.Amount / endDaysInMonth * end.Day;
-                budgets += endBudgetAmount;
-                return budgets;
-            }
+            return SameMonth(start, end) ? TheSameMonthBudgets(start, end) : DifferentMonthsBudgets(start, end);
         }
 
-        private  int FindDaysInMonth(DateTime start)
+        private decimal TheSameMonthBudgets(DateTime start, DateTime end)
+        {
+            //同月 
+            //1.找出預算
+            var budget = QueryMonthlyBudget(start);
+
+            //2.找出當月天數
+            var daysInMonth = QueryDaysInMonth(start);
+
+            //3.分配給每天 與 計算天數
+            return budget.Amount / daysInMonth * ((end - start).Days + 1);
+        }
+
+        private decimal DifferentMonthsBudgets(DateTime start, DateTime end)
+        {
+            // 計算開始月份的預算
+            var budgets = CalculateTheBudgetForTheStartMonth(start);
+
+            // 計算中間月份的預算
+            budgets = CalculateTheBudgetForTheInterimMonths(start, end, budgets);
+
+            // 計算結束月份的預算
+            budgets = CalculateTheBudgetForTheEndMonth(end, budgets);
+            return budgets;
+        }
+
+        private decimal CalculateTheBudgetForTheEndMonth(DateTime end, decimal budgets)
+        {
+            var endBudget = QueryMonthlyBudget(end);
+
+            //2.找出當月天數
+            var endDaysInMonth = QueryDaysInMonth(end);
+
+            //3.分配給每天 與 計算天數
+            var endBudgetAmount = endBudget.Amount / endDaysInMonth * end.Day;
+            budgets += endBudgetAmount;
+            return budgets;
+        }
+
+        private decimal CalculateTheBudgetForTheInterimMonths(DateTime start, DateTime end, decimal budgets)
+        {
+            var endDate = new DateTime(end.Year, end.Month, 1);
+            for(var month = start.AddMonths(1); month < endDate; month = month.AddMonths(1))
+            {
+                var budget = QueryMonthlyBudget(month);
+                if (budget == null)
+                {
+                    continue;
+                }
+                var daysInMonth = DateTime.DaysInMonth(month.Year, month.Month);
+                budgets += budget.Amount / daysInMonth * daysInMonth;
+            }
+            return budgets;
+        }
+
+        private decimal CalculateTheBudgetForTheStartMonth(DateTime start)
+        {
+            var startBudget = QueryMonthlyBudget(start);
+
+            //2.找出當月天數
+            var daysInMonth = QueryDaysInMonth(start);
+
+            //3.分配給每天 與 計算天數
+            var budgets = startBudget.Amount / daysInMonth * (daysInMonth - start.Day + 1);
+            return budgets;
+        }
+
+        private int QueryDaysInMonth(DateTime start)
         {
             return DateTime.DaysInMonth(start.Year, start.Month);
         }
