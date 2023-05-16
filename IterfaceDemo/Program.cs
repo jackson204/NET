6767ea2d66e8f046487780af93963c4d58080ecb
jackson <s97403002a@gmail.com>
2023-05-16 23:02:11 +0800

    add Iterface

---
+++ IterfaceDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace IterfaceDemo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            IFlyable bird = new Bird();
+            bird.Fly();
+        }
+    }
+
+    public interface IFlyable
+    {
+        void Fly();
+    }
+
+    public class Person : IFlyable
+    {
+        public void Fly()
+        {
+            Console.WriteLine("Person Fly");
+        }
+    }
+
+    public class Bird : IFlyable
+    {
+        public void Fly()
+        {
+            Console.WriteLine("Bird Fly");
+        }
+    }
+}
