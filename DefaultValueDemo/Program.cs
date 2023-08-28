0823ba08bb31ed4d1891755a05ec8278ccda60dc
jackson <s97403002a@gmail.com>
2023-08-28 19:58:05 +0800

    default Value

---
+++ DefaultValueDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DefaultValueDemo
+{
+    internal class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            var a = default(int);
+            var o = default(Object);
+            var s = default(string);                    
+        }
+    }
+}
